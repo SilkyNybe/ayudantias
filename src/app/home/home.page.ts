1829452cdd24955528a0ad93cf94ff60843afc4f
Dango2619 <dana.curin@duocuc.cl>
2025-05-06 12:19:42 -0400

    se agrega pagina home

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+
+interface Doctor {
+  avatar: string;
+  name: string;
+  specialty: string;
+  status: boolean;
+}
+
+interface Categories {
+  color: string;
+  bkColor: string;
+  name: string;
+  icon: string;
+}
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false
+})
+export class HomePage implements OnInit {
+
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 3.3,
+  };
+
+  doctors: Doctor[] = [
+    {
+      avatar: 'https://github.com/vagnersabadi/ionic-doctor-appointments-app/blob/main/src/assets/avatar.png?raw=true',
+      specialty: 'Cardiologist',
+      name: 'Dr. Alan C Braverman',
+      status: true
+    },
+    {
+      avatar: 'https://github.com/vagnersabadi/ionic-doctor-appointments-app/blob/main/src/assets/avatar.png?raw=true',
+      specialty: 'Orthodontist',
+      name: 'Dr. Michael Aaron',
+      status: false
+    },
+    {
+      avatar: 'https://github.com/vagnersabadi/ionic-doctor-appointments-app/blob/main/src/assets/avatar.png?raw=true',
+      specialty: 'Eye Specialist',
+      name: 'Dr. Shelley Rahman',
+      status: false
+    },
+    {
+      avatar: 'https://github.com/vagnersabadi/ionic-doctor-appointments-app/blob/main/src/assets/avatar.png?raw=true',
+      specialty: 'Orthodontist',
+      name: 'Dr. Michael Aaron',
+      status: false
+    },
+    {
+      avatar: 'https://github.com/vagnersabadi/ionic-doctor-appointments-app/blob/main/src/assets/avatar.png?raw=true',
+      specialty: 'Eye Specialist',
+      name: 'Dr. Shelley Rahman',
+      status: false
+    }
+  ];
+
+  categories: Categories[] = [
+    {
+      name: 'Heart',
+      bkColor: '#008aff',
+      color: 'white',
+      icon: 'heart-outline'
+    },
+    {
+      name: 'Brain',
+      bkColor: '#a2c3f7',
+      color: 'black',
+      icon: 'skull-outline'
+    },
+    {
+      name: 'Kidney',
+      bkColor: '#f7be3021',
+      color: 'black',
+      icon: 'heart-outline'
+    },
+    {
+      name: 'Brain',
+      bkColor: '#a2c3f7',
+      color: 'black',
+      icon: 'skull-outline'
+    },
+    {
+      name: 'Kidney',
+      bkColor: '#f7be3021',
+      color: 'black',
+      icon: 'heart-outline'
+    }
+  ];
+
+  constructor(private modalController: ModalController) { }
+
+  ngOnInit() {
+  }
+
+  // async openProfile() {
+  //   const modal = await this.modalController.create({
+  //     component: ProfilePage,
+  //   });
+  //   await modal.present();
+  // }
+
+}
