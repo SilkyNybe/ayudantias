24159df3e2dcd558ae0c431c7afdf124f908c8cf
SilkyNybe <estebanrojel17@gmail.com>
2025-05-13 00:06:39 -0400

    Se agregaron los archivos necesarios para la API REST y BD

---
+++ src/BaseDatos/index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express'
+import {PORT} from './config.js'
+import userRoutes from './routes/users.routes.js';
+import morgan from 'morgan';
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(userRoutes);
+
+app.listen(PORT)
+console.log('Server ejecutandose en el puerto', PORT);
